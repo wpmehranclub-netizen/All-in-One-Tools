5dbf09992b3fcee93ef3e7cf637db6883beadfda
Qamar <contact.qamarzaman@gmail.com>
2026-03-27 01:26:06 +0500

    Fix ThemeProvider prop types

--- frontend/src/components/ThemeProvider.tsx
+++ frontend/src/components/ThemeProvider.tsx
@@ -3,6 +3,6 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: { children: React.ReactNode, attribute?: string, defaultTheme?: string, enableSystem?: boolean, disableTransitionOnChange?: boolean }) {
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
